7a79c6275063fd583c3b0453850ba949aa908682
Renato Richard <Renatorichard2012@gmail.com>
2019-11-28 15:32:58 -0300

    correcao de erros de compatibilidade no  pakage.json

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,5 +1,5 @@
-import 'node_modules/bootstrap/dist/css/bootstrap.min.css'
-import 'node_modules/font-awesome/css/font-awesome.min.css'
+import '/home/renato/Documentos/LASIC/Guia-Do-Santuario/todo-app/frontend/node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '/home/renato/Documentos/LASIC/Guia-Do-Santuario/todo-app/frontend/node_modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
 
